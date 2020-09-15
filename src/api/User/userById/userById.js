9024c7ca458a96659ec70e833aaca25824297838
Ruslan <ruslan@appscloud.io>
2020-09-15 16:37:15 +0300

    add api actions (follow, unfollow, seeUser, searchUser and another)

--- src/api/User/userById/userById.js
+++ src/api/User/userById/userById.js
@@ -2,7 +2,6 @@ import { prisma } from "../../../../generated/prisma-client";
 export default {
     Query: {
         userById: (_, args) => {
-            console.log(_)
             const { id } = args;
             return prisma.user({ id });
         }
